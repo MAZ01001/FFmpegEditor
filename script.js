c7ba2b1e44b951c37690bdf50bb8094ea8a8c9be
MAZ <29178226+MAZ01001@users.noreply.github.com>
2024-04-01 03:42:21 +0200

    Update script.js
    more TODOs for more filter options

--- script.js
+++ script.js
@@ -67,4 +67,31 @@
     # ...
 
     FAV also note https://ffmpeg.org/ffmpeg-all.html#Quoting-and-escaping
+
+    TODO add filter: crop=WIDTH:HEIGHT:POSX:POSY
+    > add red box (CSS) to visualize final area ~ pan/zoom-able area with video elements at final size and position (CSS)
+    TODO add filter: scale=WIDTH:HEIGHT:flags=FLAG[:param0=VALUE] / scale_cuda=WIDTH:HEIGHT:interp_algo=ALGO
+    > [flags]
+    >> fast_bilinear: Select fast bilinear scaling algorithm.
+    >> bilinear: Select bilinear scaling algorithm.
+    >> bicubic: Select bicubic scaling algorithm. (default)
+    >> experimental: Select experimental scaling algorithm.
+    >> neighbor: Select nearest neighbor rescaling algorithm.
+    >> area: Select averaging area rescaling algorithm.
+    >> bicublin: Select bicubic scaling algorithm for the luma component, bilinear for chroma components.
+    >> gauss: Select Gaussian rescaling algorithm.
+    >> sinc: Select sinc rescaling algorithm.
+    >> lanczos: Select Lanczos rescaling algorithm. The default width (alpha) is 3 and can be changed by setting param0.
+    >> spline: Select natural bicubic spline rescaling algorithm.
+    >> accurate_rnd: Enable accurate rounding.
+    >> full_chroma_int: Enable full chroma interpolation.
+    >> full_chroma_inp: Select full chroma input.
+    >> bitexact: Enable bitexact output.
+    > [interp_algo]
+    >> nearest: Nearest neighbour - Used by default if input parameters match the desired output.
+    >> bilinear: Bilinear
+    >> bicubic: Bicubic - This is the default.
+    >> lanczos: Lanczos
+    TODO add filter: pad=WIDTH:HEIGHT:X:Y[:[0x|#]RRGGBB[AA]] (X/Y -1 to center ~ top/left)
+    > MP4 does not support transparency
 */
